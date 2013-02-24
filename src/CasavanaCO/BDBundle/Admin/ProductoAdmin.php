a5a600f4df92fd3172c879b113d354836d8a9a4e
Pablo Torres <pta1988@gmail.com>
2013-02-24 23:10:42 +0100

    Producto Admin

---
+++ src/CasavanaCO/BDBundle/Admin/ProductoAdmin.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace CasavanaCO\BDBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class ProductoAdmin extends Admin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+        		->add('dniGestor', 'sonata_type_model', array())
+            ->add('nombre')
+            ->add('descripcion')
+            ->add('coste')
+            ->add('margen')
+            ->add('precio')
+            ->add('categoria')
+            ->add('estado')
+            
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('nombre')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('nombre')
+        ;
+    }
+}
