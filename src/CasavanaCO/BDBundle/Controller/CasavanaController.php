7b14a08c2873e962a3c20bef8d8ad8b7b45b05ee
Pablo Torres <pta1988@gmail.com>
2013-03-05 01:40:23 +0100

    Vistas client - admin - dashboard (super admin)

--- src/CasavanaCO/BDBundle/Controller/CasavanaController.php
+++ src/CasavanaCO/BDBundle/Controller/CasavanaController.php
@@ -15,16 +15,18 @@ class CasavanaController extends Controller
      */
     public function indexAction(){
 	
-                
-		if($this->get('security.context')->isGranted('ROLE_ADMIN')){
-                    return $this->render("CasavanaCOBDBundle:Admin:admin.html.twig");
-                }
-                else if($this->get('security.context')->isGranted('ROLE_USER')){
-                    return $this->render("CasavanaCOBDBundle:Client:client.html.twig");
-                }
-                else{
-                    return $this->render("CasavanaCOBDBundle:Error:rrlogin.html.twig");
-                }
+        if($this->get('security.context')->isGranted('ROLE_SUPER_ADMIN')){
+			return $this->render("CasavanaCOBDBundle:Dashboard:dashboard.html.twig");
+		}      
+		else if($this->get('security.context')->isGranted('ROLE_ADMIN')){
+			return $this->render("CasavanaCOBDBundle:Admin:admin.html.twig");
+		}
+		else if($this->get('security.context')->isGranted('ROLE_USER')){
+			return $this->render("CasavanaCOBDBundle:Client:client.html.twig");
+		}
+		else{
+			return $this->render("CasavanaCOBDBundle:Error:rrlogin.html.twig");
+		}
 		
     }
 }
