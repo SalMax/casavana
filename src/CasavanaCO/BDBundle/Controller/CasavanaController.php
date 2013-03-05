f986cfce83da5d7b1cf876a5e936822cce0128ef
Pablo Torres <pta1988@gmail.com>
2013-03-05 01:33:38 +0100

    VIstas client y admin (app.php)

--- src/CasavanaCO/BDBundle/Controller/CasavanaController.php
+++ src/CasavanaCO/BDBundle/Controller/CasavanaController.php
@@ -6,14 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+
 class CasavanaController extends Controller
 {
     /**
      * @Route("/")
      * @Template()
      */
-    public function indexAction()
-    {
-        return $this->render('CasavanaCOBDBundle:Casavana:index.html.twig');
+    public function indexAction(){
+	
+                
+		if($this->get('security.context')->isGranted('ROLE_ADMIN')){
+                    return $this->render("CasavanaCOBDBundle:Admin:admin.html.twig");
+                }
+                else if($this->get('security.context')->isGranted('ROLE_USER')){
+                    return $this->render("CasavanaCOBDBundle:Client:client.html.twig");
+                }
+                else{
+                    return $this->render("CasavanaCOBDBundle:Error:rrlogin.html.twig");
+                }
+		
     }
 }
