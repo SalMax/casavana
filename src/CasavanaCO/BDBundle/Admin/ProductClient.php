66c517d5c4fbc2dfb12320823a573d82820f053d
Sergio Álvarez <salmaxcraft@gmail.com>
2013-03-15 13:05:03 -0400

    Corregido erro en InvoiceClient e inicio de ProductClient

---
+++ src/CasavanaCO/BDBundle/Admin/ProductClient.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace CasavanaCO\BDBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Form\FormType;
+
+class ProductAdmin extends Admin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name')
+            ->add('description')
+            ->add('cost')
+            ->add('margin')
+            ->add('price_by','choice', array('choices' => array('unit' => 'Unit', 'lb' => 'Lb')))
+            ->add('category', 'sonata_type_model_list', array('required' => false))
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('description')
+            ->add('cost')
+            ->add('margin')
+            ->add('price')
+            ->add('_action', 'actions', array(
+            'actions' => array(
+		        'edit' => array(),
+		        'delete' => array()
+				    )))
+        ;
+    }
+    
+    public function prePersist($product)
+    {
+        $product->setPrice($product->getCost()+$product->getMargin());
+        
+    }
+    
+    public function preUpdate($product)
+    {
+        $product->setPrice($product->getCost()+$product->getMargin());
+        
+    }
+}
